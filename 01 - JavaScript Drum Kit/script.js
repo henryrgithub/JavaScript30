154813ac2662c521ad05e8f820de5f9547016a0e
henryrgithub <68022539+henryrgithub@users.noreply.github.com>
2023-07-20 17:57:27 -0700

    01 wordking as intended

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,3 +1,20 @@
 document.addEventListener("keydown", (e)=>{
-    alert(`'${e.key}'`)
+    if (e.key.length > 1) return;
+    const encoded = e.key.toUpperCase().charCodeAt(0).toString();
+    const ourKey = document.querySelector(`div[data-key="${encoded}"]`)
+    if ((!ourKey)) return;
+    console.log(`Playing ${e.key}`)
+    ourKey.classList.add('playing')
+    const ourSound = document.querySelector(`audio[data-key="${encoded}"]`)
+    ourSound.currentTime = 0;
+    ourSound.play();
+});
+
+document.addEventListener("keyup", (e)=>{
+    if (e.key.length > 1) return;
+    const encoded = e.key.toUpperCase().charCodeAt(0).toString();
+    const ourKey = document.querySelector(`div[data-key="${encoded}"]`)
+    if ((!ourKey)) return;
+    console.log(`Releasing ${e.key}`)
+    ourKey.classList.remove('playing')
 });
